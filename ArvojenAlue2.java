18f9049ad85a49b1804ecce29a0d078b46f39f10
bekshoi <1907089@edu.karelia.fi>
2020-11-03 16:39:11 +0200

    hei

--- ArvojenAlue2.java
+++ ArvojenAlue2.java
@@ -1,6 +1,6 @@
 /**
  * Tehtävä 2/2
- * TEST TEST@
+ * TEST TEST@ TEST22
  * Versio 1.0 Tee ohjelma, joka pyytää käyttäjältä numeron ja toteaa
  * sopiva, jos 1-10 tai ei kelpaa, jos jotakin muuta.
  * 
